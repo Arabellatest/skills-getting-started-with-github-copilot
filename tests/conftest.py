5418c0e5b4084e0934f68a95ff8328831b32ce76
Arabellatest <v-arabellaqi@microsoft.com>
2026-09-23 07:41:17 +0000

    Add tests for activity signup and unregistration, update README and requirements

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+from copy import deepcopy
+
+import pytest
+
+from src.app import activities
+
+
+@pytest.fixture(autouse=True)
+def restore_activities():
+    original_activities = deepcopy(activities)
+
+    try:
+        yield
+    finally:
+        activities.clear()
+        activities.update(original_activities)
